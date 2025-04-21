b4cb39fa8108572d60b4e5286cf945359cf47ef0
vutlx13 <phamngocvu910@gmail.com>
2025-04-21 19:17:51 +0700

    Sửa chức năng upload

--- loai/upload_part.py
+++ loai/upload_part.py
@@ -1,21 +1,22 @@
 import cv2
 import numpy as np
+from io import BytesIO
+from PIL import Image
 
-
-def detect_face_from_upload(uploaded_file):
-    # Đọc hình ảnh
-    image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1)
+def detect_face_from_upload(file):
+    # Đọc ảnh từ file
+    img = np.array(file)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    # Chuyển ảnh từ BGR sang RGB
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-    # Chuyển đổi sang màu RGB
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    
-    # Nhận diện khuôn mặt
+    # Sử dụng CascadeClassifier để nhận diện khuôn mặt
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
+    
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    
     # Vẽ hình chữ nhật quanh khuôn mặt
     for (x, y, w, h) in faces:
-        cv2.rectangle(image_rgb, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-    return image_rgb
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img_rgb
