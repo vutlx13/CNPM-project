32c0dde4e12404ee59bb1964eb67cefd13fd22a7
vutlx13 <phamngocvu910@gmail.com>
2025-05-08 02:55:18 +0700

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -15,13 +15,23 @@ CREATE TABLE IF NOT EXISTS library_images (
 
 # Tạo bảng lịch sử người dùng
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS history_images (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS history_images_new (
     filename TEXT,
     upload_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    is_public INTEGER DEFAULT 0
+    is_public INTEGER DEFAULT 0,
+    username TEXT,
+    created_at
 )
 ''')
+
+cursor.execute('''
+INSERT INTO history_images_new (filename, upload_time, is_public, created_at)
+SELECT filename, upload_time, is_public, created_at FROM history_images
+''')
+
+cursor.execute('DROP TABLE history_images')
+
+cursor.execute('ALTER TABLE history_images_new RENAME TO history_images')
 
 conn.commit()
 conn.close()
